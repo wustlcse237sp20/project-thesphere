18bbfa8356a6721106b3369ed67602811cf29543
Michael Jesser <m.jesser@wustl.edu>
2020-04-16 23:01:12 -0500

    Signing in and creating an account logic now handled in User.java.
    Also cleared out a bunch of whitespace.

--- src/classes/User.java
+++ src/classes/User.java
@@ -1,4 +1,8 @@
 package classes;
+
+import java.io.*;
+import java.util.Scanner;
+
 public class User {
 	private String firstName;
 	private String lastName;
@@ -6,13 +10,32 @@ public class User {
 	//Eventually put a wallet object here and in constructor
 
 
-	public User(String first, String last, String emailAddress){
-		this.firstName = first;
-		this.lastName = last;
+	public User(String emailAddress){
 		this.email = emailAddress;
 
-		//eventually an sql query here to get their wallet
-
+		try {
+			File f = new File("Users/"+this.email+"/info.txt");
+			Scanner s = new Scanner(f);
+			int counter = 0;
+			while (s.hasNextLine()) {
+				String line = s.nextLine();
+				if (counter==0) {
+					this.firstName = line.split(": ")[1];
+				}
+				else if (counter==1) {
+					this.lastName = line.split(": ")[1];
+				}
+				counter++;
+			}
+			s.close();
+		}
+		catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public String getName() {
+		return this.firstName+" "+this.lastName;
 	}
 
 
@@ -38,6 +61,44 @@ public class User {
 	//dont want to store password anywhere besides db so this one is separate
 	public void updatePassword(String newPass){
 		//sql update here
+	}
+	
+	public static User createNewUser(String first, String last, String email, String password) throws IOException {
+		File u = new File("Users");
+		if (!u.exists()) {
+			u.mkdir();
+		}
+		File f = new File("Users/"+email);
+		f.mkdir();
+		
+		FileWriter passWriter = new FileWriter("./Users/"+email+"/password.txt");
+		passWriter.write(password);
+		passWriter.close();
+		
+		FileWriter infoWriter = new FileWriter("./Users/"+email+"/info.txt");
+		infoWriter.write("First Name: "+first+System.lineSeparator()+"Last Name: "+last);
+		infoWriter.close();
+		
+		return new User(email);
+	}
+	
+	public static boolean checkIfEmailUsed(String email) {
+		File f = new File("Users/"+email);
+		return f.exists();
+	}
+	
+	public static boolean checkEmailAndPassword(String email, String password) {
+		File f = new File("Users/"+email+"/password.txt");
+		try {
+			Scanner s = new Scanner(f);
+			String pass = s.nextLine();
+			s.close();
+			return password.equals(pass);
+		}
+		catch (FileNotFoundException e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
 
 }
