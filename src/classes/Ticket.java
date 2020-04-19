cee2e986ec84c6a85d9c90aaf51027fb0df4da14
ethanhillis <ethanhillis@ethans-mbp.lan>
2020-04-19 16:40:42 -0600

    new ticket class and user account page

---
+++ src/classes/Ticket.java
@@ -0,0 +1,41 @@
+package classes;
+
+public class Ticket {
+
+	private String dateAndBand;
+	private String seat;
+	
+	
+	public Ticket() {
+		
+		dateAndBand = this.dateAndBand;
+		seat = this.seat;
+		
+	}
+	
+	public String getDateAndBand() {
+		
+		return dateAndBand; 
+		
+	}
+	
+	public String getSeat() {
+		
+		return seat;
+		
+	}
+	
+	public void setDateAndBand(String d) {
+		
+		this.dateAndBand = d;
+		
+	}
+	
+	public void setSeat(String s) {
+		
+		this.seat = s;
+		
+	}
+
+}
+	
---
+++ src/ui/UserAccountPage.java
@@ -0,0 +1,137 @@
+package ui;
+import classes.*;
+
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.util.List;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SpringLayout;
+import javax.swing.SwingConstants;
+
+public class UserAccountPage {
+
+	private JFrame userAccountPageFrame;
+	private String purchasedTicket;
+	private String signedInEmail;
+	private String signedInName;
+	private List<Ticket> purchasedTicketsList;
+	private int numberOfPurchasedTickets;
+
+	
+
+
+	public UserAccountPage(String signedInName, List<Ticket> purchasedTicketsList) {
+		
+		super();
+		this.signedInName = signedInName;
+		this.purchasedTicketsList = purchasedTicketsList;
+		initialize();
+		
+	}
+	
+	public UserAccountPage(String signedInEmail) {
+		
+		super();
+		this.signedInName = signedInEmail;
+		initialize();
+		
+	}
+	
+
+	public JFrame getUserAccountPageFrame() {
+		return userAccountPageFrame;
+	}
+
+	public void setUserAccountPageFrame(JFrame userAccountPageFrame) {
+		this.userAccountPageFrame = userAccountPageFrame;
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		userAccountPageFrame = new JFrame();
+		userAccountPageFrame.setBounds(100, 100, 1100, 600);
+		userAccountPageFrame.setResizable(false);
+		userAccountPageFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		SpringLayout springLayout = new SpringLayout();
+		userAccountPageFrame.getContentPane().setLayout(springLayout);
+		
+		JLabel yourAccountPageLabel = new JLabel("Your Account Page");
+		yourAccountPageLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
+		yourAccountPageLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		springLayout.putConstraint(SpringLayout.NORTH, yourAccountPageLabel, 10, SpringLayout.NORTH, userAccountPageFrame.getContentPane());
+		springLayout.putConstraint(SpringLayout.WEST, yourAccountPageLabel, 10, SpringLayout.WEST, userAccountPageFrame.getContentPane());
+		springLayout.putConstraint(SpringLayout.SOUTH, yourAccountPageLabel, 52, SpringLayout.NORTH, userAccountPageFrame.getContentPane());
+		springLayout.putConstraint(SpringLayout.EAST, yourAccountPageLabel, 270, SpringLayout.WEST, userAccountPageFrame.getContentPane());
+		userAccountPageFrame.getContentPane().add(yourAccountPageLabel);
+		
+		
+		try {
+			
+			numberOfPurchasedTickets = purchasedTicketsList.size();
+			
+		}
+		
+		catch(Exception e) {
+			
+			numberOfPurchasedTickets = 0;
+			
+		}
+		
+		JLabel upcomingTicketsLabel = new JLabel("Your Upcoming Tickets: " + numberOfPurchasedTickets);
+		springLayout.putConstraint(SpringLayout.NORTH, upcomingTicketsLabel, -435, SpringLayout.SOUTH, userAccountPageFrame.getContentPane());
+		springLayout.putConstraint(SpringLayout.WEST, upcomingTicketsLabel, 75, SpringLayout.WEST, userAccountPageFrame.getContentPane());
+		springLayout.putConstraint(SpringLayout.SOUTH, upcomingTicketsLabel, -419, SpringLayout.SOUTH, userAccountPageFrame.getContentPane());
+		upcomingTicketsLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
+		userAccountPageFrame.getContentPane().add(upcomingTicketsLabel);
+		
+		JLabel firstPurchasedTicketLabel;
+		
+		if(numberOfPurchasedTickets == 0) {
+			
+			firstPurchasedTicketLabel = new JLabel("No tickets purchased yet");
+			
+			
+		}else {
+			
+			firstPurchasedTicketLabel = new JLabel(purchasedTicketsList.get(0).getDateAndBand() + "; " + purchasedTicketsList.get(0).getSeat());
+			
+		}
+		
+		springLayout.putConstraint(SpringLayout.NORTH, firstPurchasedTicketLabel, -387, SpringLayout.SOUTH, userAccountPageFrame.getContentPane());
+		springLayout.putConstraint(SpringLayout.WEST, firstPurchasedTicketLabel, 87, SpringLayout.WEST, userAccountPageFrame.getContentPane());
+		springLayout.putConstraint(SpringLayout.SOUTH, firstPurchasedTicketLabel, -371, SpringLayout.SOUTH, userAccountPageFrame.getContentPane());
+		userAccountPageFrame.getContentPane().add(firstPurchasedTicketLabel);
+		
+		JLabel signedInAsLabel = new JLabel("Signed in as: " + signedInName);
+		signedInAsLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		springLayout.putConstraint(SpringLayout.NORTH, signedInAsLabel, 24, SpringLayout.NORTH, userAccountPageFrame.getContentPane());
+		springLayout.putConstraint(SpringLayout.WEST, signedInAsLabel, -789, SpringLayout.EAST, userAccountPageFrame.getContentPane());
+		springLayout.putConstraint(SpringLayout.EAST, signedInAsLabel, -547, SpringLayout.EAST, userAccountPageFrame.getContentPane());
+		userAccountPageFrame.getContentPane().add(signedInAsLabel);
+		
+		
+		if(numberOfPurchasedTickets > 1) {
+			
+			for(int i = 0; i < purchasedTicketsList.size(); i++) {
+				
+				JLabel label = new JLabel(purchasedTicketsList.get(i).getDateAndBand() + "; " + purchasedTicketsList.get(i).getSeat());
+				springLayout.putConstraint(SpringLayout.NORTH, label, (-387 + i*60), SpringLayout.SOUTH, userAccountPageFrame.getContentPane());
+				springLayout.putConstraint(SpringLayout.WEST, label, 0, SpringLayout.WEST, firstPurchasedTicketLabel);
+				springLayout.putConstraint(SpringLayout.SOUTH, label, (-371 + i*60), SpringLayout.SOUTH, userAccountPageFrame.getContentPane());
+				userAccountPageFrame.getContentPane().add(label);
+				
+				
+			}
+			
+		}
+		
+		
+		
+	}
+	
+	
+}
