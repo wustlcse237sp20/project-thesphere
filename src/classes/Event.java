d6c437f4861478d558b0c0ee0e81e38b6a7b6c61
jessica-king <35541127+jessica-king@users.noreply.github.com>
2020-04-19 15:06:02 -0700

    test commit

---
+++ src/classes/Event.java
@@ -0,0 +1,16 @@
+package classes;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Event{
+	private String eventName;
+	private String eventDate;
+	private String eventTime;
+	private String eventOpener;
+	private int rowNumber;
+	private int seatNumber;
+	private int ticketPrice;
+
+	
+}
