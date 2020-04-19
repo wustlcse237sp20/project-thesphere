e858a2292b64acd1d48c32dd69a527809d7733e9
jessica-king <35541127+jessica-king@users.noreply.github.com>
2020-04-19 15:15:20 -0700

    test commit

--- src/classes/Event.java
+++ src/classes/Event.java
@@ -1,16 +1,49 @@
 package classes;
 
-import java.io.*;
-import java.util.Scanner;
 
 public class Event{
 	private String eventName;
 	private String eventDate;
 	private String eventTime;
-	private String eventOpener;
 	private int rowNumber;
 	private int seatNumber;
 	private int ticketPrice;
-
 	
+	public String getEventName() {
+		return eventName;
+	}
+	public void setEventName(String eventName) {
+		this.eventName = eventName;
+	}
+	public String getEventDate() {
+		return eventDate;
+	}
+	public void setEventDate(String eventDate) {
+		this.eventDate = eventDate;
+	}
+	public String getEventTime() {
+		return eventTime;
+	}
+	public void setEventTime(String eventTime) {
+		this.eventTime = eventTime;
+	}
+	public int getRowNumber() {
+		return rowNumber;
+	}
+	public void setRowNumber(int rowNumber) {
+		this.rowNumber = rowNumber;
+	}
+	public int getSeatNumber() {
+		return seatNumber;
+	}
+	public void setSeatNumber(int seatNumber) {
+		this.seatNumber = seatNumber;
+	}
+	public int getTicketPrice() {
+		return ticketPrice;
+	}
+	public void setTicketPrice(int ticketPrice) {
+		this.ticketPrice = ticketPrice;
+	}
+
 }
